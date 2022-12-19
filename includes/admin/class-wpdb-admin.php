87a226dfea8d716087c2ff7b004a7b26fda8e2a3
Akshay Wali <akshaywali@live.in>
2022-12-19 14:39:55 +0530

    #16  support sticky icon

--- includes/admin/class-wpdb-admin.php
+++ includes/admin/class-wpdb-admin.php
@@ -682,7 +682,7 @@ class Wpdb_Admin {
 										Before
 										using this option make sure that you have save your database backup on safe
 										place.</p>
-									<p>The disk that your backup is saved on doesn’t have enough free space? Backup disk
+									<p>The disk that your backup is saved on doesn't have enough free space? Backup disk
 										is
 										almost full? Low disk space for backup? Backup failed due to lack of space? As
 										you
@@ -690,7 +690,7 @@ class Wpdb_Admin {
 										of
 										disk space is limited, so your backup disk will run out of space quickly or
 										someday.
-										It is a real pain to manually delete old backups. Don’t worry about it. WP
+										It is a real pain to manually delete old backups. Don't worry about it. WP
 										Database
 										Backup makes it easy to delete old/temparary backup files using this option.</p>
 
@@ -1347,10 +1347,15 @@ class Wpdb_Admin {
 
 
 			</div>
-
+			</div>
+			
+	
 		</div>
-		</div>
-
+		<a aria-label="Open Support" aria-expanded="false" aria-controls="HSBeaconContainerFrame" class="wpdbbkp-support-button" href="https://magazine3.company/contact/" target="_blank">
+				<span class="wpdbbkp-support-icon">
+					<svg width="24" height="22" xmlns="http://www.w3.org/2000/svg"><path d="M20.347 20.871l-.003-.05c0 .017.001.034.003.05zm-.243-4.278a2 2 0 0 1 .513-1.455c1.11-1.226 1.383-2.212 1.383-4.74C22 5.782 18.046 2 13.125 2h-2.25C5.954 2 2 5.78 2 10.399c0 4.675 4.01 8.626 8.875 8.626h2.25c.834 0 1.606-.207 3.212-.798a2 2 0 0 1 1.575.083l2.355 1.161-.163-2.878zM10.875 0h2.25C19.13 0 24 4.656 24 10.399c0 2.6-.25 4.257-1.9 6.08l.243 4.279c.072.845-.807 1.471-1.633 1.162l-3.682-1.816c-1.212.446-2.527.921-3.903.921h-2.25C4.869 21.025 0 16.142 0 10.4 0 4.656 4.869 0 10.875 0z" fill="#FFF"></path></svg>
+				</span>
+			</a>
 		<?php
 	}
 

bde4699bcf90320763dd9961c78a22bb32e98b48
Akshay Wali <akshaywali@live.in>
2023-06-06 14:49:20 +0530

    update readme and version

--- wp-database-backup.php
+++ wp-database-backup.php
@@ -3,7 +3,7 @@
  * Plugin Name: Backup For WP  
  * Plugin URI:https://wordpress.org/plugins/wp-database-backup
  * Description: This plugin helps you to create/restore Unlimited  Wordpress Database & Files backup.
- * Version: 6.2
+ * Version: 6.3
  * Author: Backup for WP
  * Author URI: https://backupforwp.com/
  * Text Domain: wpdbbkp
@@ -47,7 +47,7 @@ if ( ! class_exists( 'WPDatabaseBackup' ) ) :
 		 *
 		 * @var string
 		 */
-		public $version = '6.2';
+		public $version = '6.3';
 
 		/**
 		 * Plugin instance

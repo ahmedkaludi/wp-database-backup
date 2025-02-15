a05d715f78b5b09d05392b6b6fdbe5d348210136
CarolusFPV <38926691+CarolusFPV@users.noreply.github.com>
2025-02-15 17:12:31 +0100

    Update sendaway.php
    fix SFTP/sendaway.php: Define $result
    - Retrieve SFTP options and establish connection before authentication.
    - Implemented key/password authentication

--- includes/admin/Destination/SFTP/sendaway.php
+++ includes/admin/Destination/SFTP/sendaway.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Destination ftp
+ * Destination SFTP
  *
  * @package wpdbbkp
  */
@@ -9,20 +9,50 @@
 
 require __DIR__ . '/vendor/autoload.php';
 use phpseclib3\Net\SFTP;
+use phpseclib3\Crypt\PublicKeyLoader;
 
-$wp_upload_dir = wp_upload_dir();
-$wp_upload_dir['basedir'] = str_replace( '\\', '/', $wp_upload_dir['basedir'] );
-$remotefile               = $directory . '/' . $filename;
-$localfile                = trailingslashit( $wp_upload_dir['basedir'] . '/db-backup' ) . $filename;
-if ( $result ) {
-	$success = $sftp->put($remotefile, $localfile, SFTP::SOURCE_LOCAL_FILE | SFTP::RESUME_START);
-	if ( $success ) {
-		$args[2] = $args[2] . '<br> '.esc_html__('Upload Database Backup on SFTP','wpdbbkp') . $host;
-		$args[4] .= 'SFTP, ';
-	}
+// Retrieve SFTP details
+$wpdbbkp_sftp_details = get_option('wp_db_backup_sftp_details', array());
+$host       = isset($wpdbbkp_sftp_details['host']) ? $wpdbbkp_sftp_details['host'] : '';
+$port       = isset($wpdbbkp_sftp_details['port']) ? $wpdbbkp_sftp_details['port'] : 22;
+$user       = isset($wpdbbkp_sftp_details['username']) ? $wpdbbkp_sftp_details['username'] : '';
+$pass       = isset($wpdbbkp_sftp_details['password']) ? $wpdbbkp_sftp_details['password'] : '';
+$pkey       = isset($wpdbbkp_sftp_details['sftp_key']) ? base64_decode($wpdbbkp_sftp_details['sftp_key']) : '';
+$key_pass   = isset($wpdbbkp_sftp_details['key_password']) ? $wpdbbkp_sftp_details['key_password'] : false;
+$directory  = isset($wpdbbkp_sftp_details['directory']) ? $wpdbbkp_sftp_details['directory'] : '';
+if ( '' === $directory ) {
+    $directory = '/';
 }
-// Close connection to host.
-if($sftp){
-	$sftp->disconnect();
-}
+$auth_type  = isset($wpdbbkp_sftp_details['auth_type']) ? $wpdbbkp_sftp_details['auth_type'] : 'password';
 
+if ( ! empty( $host ) && ! empty( $user ) && ( ! empty( $pass ) || ( 'key' === $auth_type && ! empty( $pkey ) ) ) ) {
+    $sftp = new SFTP( $host, $port );
+
+    if ( $sftp ) {
+        // Authenticate
+        if ( 'key' === $auth_type ) {
+            $key    = PublicKeyLoader::load( $pkey, $key_pass );
+            $result = $sftp->login( $user, $key );
+        } else {
+            $result = $sftp->login( $user, $pass );
+        }
+
+        // Upload file.
+        if ( $result ) {
+            $wp_upload_dir                  = wp_upload_dir();
+            $wp_upload_dir['basedir']       = str_replace( '\\', '/', $wp_upload_dir['basedir'] );
+            $remotefile                     = $directory . '/' . $filename;
+            $localfile                      = trailingslashit( $wp_upload_dir['basedir'] . '/db-backup' ) . $filename;
+            $success                        = $sftp->put( $remotefile, $localfile, SFTP::SOURCE_LOCAL_FILE | SFTP::RESUME_START );
+
+            if ( $success ) {
+                $args[2] = $args[2] . '<br> ' . esc_html__( 'Upload Database Backup on SFTP', 'wpdbbkp' ) . ' ' . $host;
+                $args[4] .= 'SFTP, ';
+            }
+        }
+    }
+
+    if ( isset( $sftp ) && $sftp ) {
+        $sftp->disconnect();
+    }
+}
